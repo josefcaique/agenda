558aa742c89a175f1521d48206edea52714a4101
josefcaique <jcaique10@gmail.com>
2024-03-07 23:18:20 -0300

    add new contact screen

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const route = express.Router()
 const homeController = require('./src/controllers/homeController')
 const loginController = require('./src/controllers/loginController')
+const contactController = require('./src/controllers/contactController')
 
 //Home routes
 route.get('/', homeController.index)
@@ -11,5 +12,9 @@ route.get('/login/index', loginController.index)
 route.post('/login/register', loginController.register)
 route.post('/login/login', loginController.login)
 route.get('/login/logout', loginController.logout)
+
+//Contact routes
+route.get('/contact/index', contactController.index)
+
 
 module.exports = route
